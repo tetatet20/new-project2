96587665b0ff0f214e4d75c1a278053d3f5e67d4
tetatet20 <romanseregicev@gmail.com>
2024-05-04 16:06:12 +0300

    drawer-layout-finish

--- vue-sneakeers/src/components/Drawer.vue
+++ vue-sneakeers/src/components/Drawer.vue
@@ -9,6 +9,24 @@ import DrawerHead from './DrawerHead.vue'
     <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
       <DrawerHead />
       <CartItemList />
+      <div class="flex flex-col gap-4 my-6">
+        <div class="flex gap-2">
+          <span>Итого</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>12900 р</b>
+        </div>
+        <div class="flex gap-2 items-end">
+          <span>Налог 5%</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>900 р</b>
+        </div>
+        <button
+          disabled=""
+          class="cursor-pointer disabled:bg-slate-400 transition bg-lime-500 w-full rounded-xl py-3 text-white hover:bg-lime-600 active:bg-lime-700"
+        >
+          Оформить заказ
+        </button>
+      </div>
     </div>
   </div>
 </template>
